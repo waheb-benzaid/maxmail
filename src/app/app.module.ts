a8b9404624084d7d62acb4030afd5f6909044747
Wahéb <waheb.benzaid@gmail.com>
2022-07-13 16:48:46 +0100

    Add Firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent],
@@ -26,6 +29,8 @@ import { MatButtonModule } from '@angular/material/button';
     ReactiveFormsModule,
     MatInputModule,
     MatButtonModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: false,
+  firebase: {
+    projectId: 'npi-maxmail',
+    appId: '1:539350298323:web:f9793d960530dbba66ae26',
+    storageBucket: 'npi-maxmail.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCcApx0Xw9-g-t9RxZehwWPwL_qj1ABuhY',
+    authDomain: 'npi-maxmail.firebaseapp.com',
+    messagingSenderId: '539350298323',
+    measurementId: 'G-6CYEKG6NPC',
+  },
 };
