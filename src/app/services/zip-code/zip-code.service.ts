c3c9e79672a2bda9c52738f9ee49ba53a1601251
waheb-benzaid <waheb.benzaid@gmail.com>
2022-10-04 17:44:44 +0100

    create zip-code CRUD ops

--- src/app/services/zip-code/zip-code.service.ts
+++ src/app/services/zip-code/zip-code.service.ts
@@ -1,8 +1,60 @@
 import { Injectable } from '@angular/core';
+import {
+  collection,
+  Firestore,
+  doc,
+  updateDoc,
+  deleteDoc,
+  collectionData,
+} from '@angular/fire/firestore';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { from, Observable } from 'rxjs';
+import { ZipCode } from 'src/app/models/Zipcode.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ZipCodeService {
-  constructor() {}
+  constructor(private firestoreDB: Firestore, private afs: AngularFirestore) {}
+
+  saveZipcode(zipcodeFields: ZipCode) {
+    let id = this.afs.createId();
+    return from(this.afs.collection('mail_zipcode').doc(id).set(zipcodeFields));
+  }
+
+  getAllZipcodesObjects() {
+    return from(
+      collectionData(collection(this.firestoreDB, 'mail_zipcode'), {
+        idField: 'id',
+      }) as Observable<ZipCode[]>
+    );
+  }
+
+  getAllZipcodes() {
+    let zipcodesNumbers: string[] = [];
+    this.getAllZipcodesObjects().subscribe((zipcodeObject) => {
+      for (const zipCode of <ZipCode[]>zipcodeObject) {
+        zipcodesNumbers.push(zipCode.zipNumber);
+      }
+    });
+    return zipcodesNumbers;
+  }
+
+  updateZipcode(id: string, dataToUpdate: any) {
+    const zipCodeToUpdate = doc(this.firestoreDB, `mail_zipcode`, id);
+    return from(
+      updateDoc(zipCodeToUpdate, dataToUpdate)
+        .then(() => {
+          console.log('Data updated');
+        })
+        .catch((err) => {
+          console.log(err.message);
+        })
+    );
+  }
+
+  deleteZipcode(id: string) {
+    let zipCodeToDelete = doc(this.firestoreDB, `mail_zipcode/${id}`);
+    return from(deleteDoc(zipCodeToDelete));
+  }
 }
