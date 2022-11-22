cd8a33db3d1df235ea124161034116ade8bc11c5
waheb-benzaid <waheb.benzaid@gmail.com>
2022-11-22 14:57:37 +0100

    add environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    projectId: 'npi-maxmail',
-    appId: '1:539350298323:web:f9793d960530dbba66ae26',
-    storageBucket: 'npi-maxmail.appspot.com',
-    locationId: 'us-central',
     apiKey: 'AIzaSyCcApx0Xw9-g-t9RxZehwWPwL_qj1ABuhY',
     authDomain: 'npi-maxmail.firebaseapp.com',
+    projectId: 'npi-maxmail',
+    storageBucket: 'npi-maxmail.appspot.com',
     messagingSenderId: '539350298323',
+    appId: '1:539350298323:web:f9793d960530dbba66ae26',
     measurementId: 'G-6CYEKG6NPC',
   },
 };
