704c6f13aaba0b17eb72d95cc434eec341905081
aheb-benzaid <waheb.benzaid@gmail.com>
2023-01-06 22:05:44 +0100

    fix recent created campaigns dashboard

--- src/app/components/dashboard/most-recent-campaigns/most-recent-campaigns.component.ts
+++ src/app/components/dashboard/most-recent-campaigns/most-recent-campaigns.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { Campaign } from 'src/app/models/Campaign.model';
 import { CampaignService } from 'src/app/services/campaign/campaign.service';
 
 @Component({
@@ -6,11 +8,23 @@ import { CampaignService } from 'src/app/services/campaign/campaign.service';
   templateUrl: './most-recent-campaigns.component.html',
   styleUrls: ['./most-recent-campaigns.component.css'],
 })
-export class MostRecentCampaignsComponent implements OnInit {
+export class MostRecentCampaignsComponent implements OnInit, OnDestroy {
   constructor(private campaignService: CampaignService) {}
-
-  public recentCampaigns$ =
-    this.campaignService.getMostRecentCreatedCampaigns();
-
-  ngOnInit(): void {}
+  public recentCampaigns$!: Subscription;
+  public campaigns: Campaign[] = [];
+  ngOnInit(): void {
+    this.campaigns.length = 0;
+    this.recentCampaigns$ = this.campaignService
+      .getMostRecentCreatedCampaigns()
+      .subscribe((res) => {
+        res.forEach((e) => {
+          this.campaigns.push(e.payload.doc.data());
+        });
+      });
+  }
+  ngOnDestroy(): void {
+    if (this.recentCampaigns$) {
+      this.recentCampaigns$.unsubscribe();
+    }
+  }
 }
